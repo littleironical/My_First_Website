f22344531c45977bd42e0130a5d37062d38015bc
Hardik kumar <hardik.kumar18feb@gmail.com>
2020-04-13 02:09:41 +0530

    commented

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-var height, width;
+// var height, width;
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    height = MediaQuery.of(context).size.height;
-    width = MediaQuery.of(context).size.width;
+    // height = MediaQuery.of(context).size.height;
+    // width = MediaQuery.of(context).size.width;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         accentColor: Colors.white
       ),
       home: Scaffold(
         body: Padding(
-          padding: EdgeInsets.all(height*0.1),
+          padding: EdgeInsets.all(20.0),
           child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(30.0)),
               border: Border.all(
-                color: Colors.grey,
+                color: Colors.blue,
                 width: 10.0,
                 style: BorderStyle.solid,
               ),
