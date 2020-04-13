9374f9d490c8b7ef82eb4edf464e607dc730cff5
Hardik kumar <hardik.kumar18feb@gmail.com>
2020-04-13 20:15:11 +0530

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,25 @@ class MyApp extends StatelessWidget {
         body: Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
-                colors: [Colors.orange, Colors.yellow, Colors.orange],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
-              )
+                stops: [0.5, 1],
+                colors: [Colors.orange, Colors.yellow],
+              ),
             ),
-            // child: Container(
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.all(Radius.circular(30.0)),
-            //     border: Border.all(
-            //       color: Color(0xFF494D55),
-            //       width: 10.0,
-            //       style: BorderStyle.solid,
-            //     ),
-            //   ),
-            //   alignment: Alignment.center,
-            // ),
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                border: Border.all(
+                  color: Color(0xFF494D55),
+                  width: 10.0,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              alignment: Alignment.center,
+            ),
           ),
         ),
     );
